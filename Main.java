3a3f3dcf865f5eddd20d433c678910c943f056d7
Denis <chernov-denis21@mail.ru>
2023-12-14 12:43:16 +0300

    Updated the main.java file in the main branch for the third time

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это добавлено в ветке main во второй раз");
+        System.out.print("Это добавлено в ветке main в третий раз");
     }
 }
